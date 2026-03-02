ee1777db60fa2902d04a198d9c64c4e3a0d6b81d
DevSete7 <faelsilverio_20@hotmail.com>
2026-03-02 00:32:32 +0000

    Aplicando recursividade

--- xadrez.c
+++ xadrez.c
@@ -1,55 +1,61 @@
 #include <stdio.h>
 
+void moverTorre(int casas){                 //Recursividade para mover a torre
+    if (casas > 0)
+    {
+        printf("Direita \n");
+        moverTorre(casas - 1);
+    }
+}
+void moverRainha(int casas){                //Recurssividade para mover a rainha
+    if (casas > 0)
+    {
+        printf("Esquerda \n");
+        moverRainha(casas - 1);
+    }
+}
+
 int main (){
 
     //Definição de variáveis
 
-    int torre, rainha = 0, bispo = 0, cavalo = 1;
+    int torre, rainha = 0, bispo = 0;
 
     //Movimento da torre - 5 casas a direita
 
+    printf("\n");
     printf("Movimentação da torre: \n");
+    moverTorre(5);                          //Chamando recursividade
 
-    for (torre = 0; torre < 5; torre++)
-    {
-        printf("Direita \n");
-    }
     //Movimento do bispo - 5 casas diagonal para cima a direita
 
+    printf("\n");
     printf("Movimentação do bispo: \n");
 
-    while (bispo < 5)
+    for (bispo = 0; bispo < 1 ; bispo++)    //Loop aninhado utilizando (for)
     {
-        printf("Cima Direita\n");
-        bispo++;
+        for (int i = 0; i < 5; i++)
+        {
+            printf("Cima Direita \n");
+        }
     }
+    
     //Movimento da rainha - 8 casas para a esquerda
 
+    printf("\n");
     printf("Movimento da Rainha: \n");
+    moverRainha(8);                         //Chamando recursividade
 
-    do
-    {
-        printf("Esquerda \n");
-        rainha++;
-
-    } while (rainha < 8);
-
-    //Movimento do cavalo - 2 casas para baixo e 1 para esquerda
+    //Movimento do cavalo - 2 casas para cima e 1 para direita
 
     printf(" \n");
     printf("Movimento do Cavalo: \n");
 
-    while (cavalo <= 1) 
+    for (int cavalo = 1, i = 0; i < 2; i++) //Loop aninhado complexo aplicando múltiplas variáveis
     {
-        for (int i = 0; i < 2; i++)
-        {
-            printf("Baixo \n");
-        }
-        cavalo++;
-        printf("Esquerda");
-        printf(" \n");
+        printf("Cima \n");
     }
-    
+        printf("Direita \n");
     
     return 0;
 }
